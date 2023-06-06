47ce3cc6f27b26259ba95f0c7c808dc68f88af02
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-06-07 01:37:35 +0300

    Finalisation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for number in range(10):
+    for i in range(number + 1, 10):
+        if number == 8 and i == 9:
+            print("{}{}" .format(number, i))
+        else:
+            print("{}{}" .format(number, i), end=", ")
