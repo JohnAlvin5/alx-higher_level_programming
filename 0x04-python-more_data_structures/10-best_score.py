1351b8239eda74aca32bf7bccae4d9b3fd184710
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-06-14 23:53:04 +0300

    Pycodestyle correction on task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,6 @@ def best_score(a_dictionary):
         my_list = list(a_dictionary.keys())
         score = 0
         biggest = ""
-        
         for i in my_list:
             if a_dictionary[i] > score:
                 score = a_dictionary[i]
