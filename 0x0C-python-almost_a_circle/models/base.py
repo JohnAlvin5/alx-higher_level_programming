636fe68bc7d437ea90c57d7be6eb528b7579a4f1
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-07-17 23:31:53 +0300

    Complete submission before correction and tests

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""defines a class base"""
+import json
+
+
+class Base:
+    """defines a class base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a
+        file
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as a_file:
+            if list_objs is None:
+                a_file.write("")
+            else:
+                list_dicts = [x.to_dictionary() for x in list_objs]
+                a_file.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation
+        json_string
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set
+        """
+        new = cls(5, 5)
+        new.update(**dictionary)
+        return new
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+"""defines a rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """defines a cass rectangle inheriting from base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """defines the width property of a rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width of a rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """defines the height propety of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height of a rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def y(self):
+        """defines the property of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """sets the y property"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    @property
+    def x(self):
+        """defines the property of x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """sets the x property"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    def area(self):
+        """ return the area of a rectangle"""
+        return (self.__height * self.__width)
+
+    def display(self):
+        """prints in stdout the rectangle instance with the character #"""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+        for h in range(self.height):
+            for w in range(self.width):
+                print("#", end="")
+            print()
+
+    def update(self, *args, **kwargs):
+        """assigns an arguement to each attribute"""
+        if args and len(args) != 0:
+            index = 0
+            for arg in args:
+                if index == 0:
+                    self.id = arg
+                elif index == 1:
+                    self.width = arg
+                elif index == 2:
+                    self.height = arg
+                elif index == 3:
+                    self.x = arg
+                elif index == 4:
+                    self.y = arg
+                index += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """prints the string representation of a rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
