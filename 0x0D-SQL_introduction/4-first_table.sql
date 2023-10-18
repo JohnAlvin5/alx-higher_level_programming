7e8489dcb825670601560650091776d11fdf096c
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-10-18 21:45:57 +0300

    Task 3

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- creates a table of a given name in the current database
+CREATE TABLE first_table (
+	id INT,
+	name VARCHAR(256)
+);
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- prints the full description of a named table in the database
+SHOW TABLE first_table;
