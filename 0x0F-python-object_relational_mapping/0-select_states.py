2853eff91f47682bc3d67e3bea86f08a2051b7c3
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-11-20 16:52:06 +0300

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
             user = sys.argv[1],
-            passwd = sys.argv[2],
+            passwd = "Alvin254.5",
             db = sys.argv[3]
             )
 
