04bfc3d803f373bc9d0f9d92864f8d1278d2a382
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-11-20 17:14:18 +0300

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Lists all states with a name starting with upper N from the
+    database hbtn_0e_0_usa."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    curs = db.cursor()
+    curs.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id`")
+    for row in curs.fetchall():
+            print(row)
