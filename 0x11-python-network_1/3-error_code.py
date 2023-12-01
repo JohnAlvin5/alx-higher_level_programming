54d24785e2063ebfa1ecd59981a23a8836144f12
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-12-02 02:28:38 +0300

    Correction

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,7 +10,7 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
-    
+
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
