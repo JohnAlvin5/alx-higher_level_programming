0e94257497d34937e4f5a49bbdb7ba4626346457
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-12-02 02:23:10 +0300

    Correction

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" takes in a letter and sends a POST request to 
+""" takes in a letter and sends a POST request to
     http://0.0.0.0:5000/search_user with the letter as a parameter
 """
 import sys
